1988bd49ba1c0a694b8b0f38154bda1f1968784c
nishisaxenatct <45178342+nishisaxenatct@users.noreply.github.com>
2019-01-22 16:31:41 -0600

    Commit the selenium first project

---
+++ Maven01/src/test/java/LearnSeleaniumWithMaven/Maven01/DemoJenkins.java
@@ -0,0 +1,11 @@
+package LearnSeleaniumWithMaven.Maven01;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkins {
+  @Test
+  public void test1() {
+	  
+	  System.out.println("Hello Jenkins");
+  }
+}
